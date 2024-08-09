e164faeb5db6e956f9bc090e850006a7f2974a1f
Михаил Абрамов <aie2061@yandex.ru>
2024-08-09 17:43:00 +0300

    update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'hehehecats.ddns.net', '158.160.93.78' ]
+ALLOWED_HOSTS = ['localhost', 'hehehecats.ddns.net', '158.160.93.78', '127.0.0.1:8080']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
